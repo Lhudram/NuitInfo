00f6770d99a98e0738fcf6f651fadd862a7cb5a1
KhiOui <rchrd.tom8636@gmail.com>
2017-12-08 01:33:42 +0100

    Ajout Konami
    Faut changer konami.jpg
    Ajouter l'image quelque part de le html avec l'id "konami"

---
+++ sith/js/konami.js
@@ -0,0 +1,16 @@
+myAudio = new Audio('audio/konamiSound.ogg');
+myAudio.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
+
+cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
+  myAudio.play();
+  var konami = document.getElementById("konami").style;
+  konami.opacity = 1;
+  konami.width = '200vh';
+  konami.height = '200vh';
+  konami.marginTop = '-100vh';
+  konami.marginLeft = '-100vh';
+  }
+);
