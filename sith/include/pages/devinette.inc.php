3f22a3b0f5acdc905281f9c3851b0bef46b48128
Reveillere Pierrick <pierrick.reveillere@etu.unilim.fr>
2017-12-08 06:38:21 +0100

    css 404

--- sith/include/pages/devinette.inc.php
+++ sith/include/pages/devinette.inc.php
@@ -1,38 +1,40 @@
 
-  <script type="text/javascript">
+<script type="text/javascript">
 
-    var num = 404;
-    var NbEssais = 0;
+var num = 404;
+var NbEssais = 0;
 
-    function Devine() {
-      var choisi = document.getElementById('devine').value;
-      NbEssais++;
-      status = "Nombre d'essais : " + NbEssais;
-      if (choisi < num){
-          var original = document.getElementById('aide');
-				    var modifie = 'Non, le nombre est plus grand.';
-				    original.innerHTML = modifie;
-          }
-      if (choisi > num){
-        var original = document.getElementById('aide');
-        var modifie = 'Non, le nombre est plus petit.';
-        original.innerHTML = modifie;
-      }
-      if (choisi == num) {
-          var original = document.getElementById('aide');
-          var modifie = 'Non, le nombre est plus petit.';
-          original.innerHTML = modifie;
-          window.alert("Correct ! Vous avez trouvé en " + NbEssais + "essais.");
-          location.reload();
-      }
-    }
+function Devine() {
+	var choisi = document.getElementById('devine').value;
+	NbEssais++;
+	status = "Nombre d'essais : " + NbEssais;
+	if (choisi < num){
+		var original = document.getElementById('aide');
+		var modifie = 'Non, le nombre est plus grand.';
+		original.innerHTML = modifie;
+	}
+	if (choisi > num){
+		var original = document.getElementById('aide');
+		var modifie = 'Non, le nombre est plus petit.';
+		original.innerHTML = modifie;
+	}
+	if (choisi == num) {
+		var original = document.getElementById('aide');
+		var modifie = 'Non, le nombre est plus petit.';
+		original.innerHTML = modifie;
+		window.alert("Correct ! Tu as trouvé en " + NbEssais + " essais.");
+		location.reload();
+	}
+}
 
-  </script>
+</script>
 
-  <h2>Une petite partie ?</h2>
-  <p>Trouve le bon nombre</p>
-  <hr>
-  <b>Votre choix :</b>
-  <input TYPE="text" id="devine" SIZE="5">
-  <button VALUE="Essayer" onClick="Devine();">Cliquez sur moi :)</button>
-  <p id="aide" ></p>
+<h2>Une petite partie ?</h2>
+<p>Trouve le bon nombre</p>
+<hr>
+<div id="devinette">
+	<b>Votre choix :</b>
+	<input TYPE="text" id="devine" SIZE="5">
+	<button VALUE="Essayer" onClick="Devine();">Tester</button>
+	<p id="aide" ></p>
+</div>
